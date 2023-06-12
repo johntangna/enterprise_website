fb4cce69325f7ebec9c0f2ddb4a99db1a93c43dd
williemwang <1092473788@qq.com>
2023-06-12 19:53:44 +0800

    feat all

---
+++ Home/.vuepress/components/EcommerceInstruction.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <ul class="ul_class">
+      <li v-for="(item, index) in eCommerceIntruction" :key="index">
+        <span v-html="item.content"></span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ECommerceIntruction",
+  data() {
+    return {
+      eCommerceIntruction: [
+        {
+          content: "",
+        },
+      ],
+      currentIndex: 0,
+    };
+  },
+  watch: {
+    pageData: {
+      handler(newV) {
+        this.updateData(newV.eCommerceIntruction);
+      },
+      deep: true,
+    },
+  },
+  methods: {
+    updateData(data) {
+      this.eCommerceIntruction = data.arr;
+    },
+  },
+  mounted() {
+    this.updateData(this.pageData.eCommerceIntruction);
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+.ul_class {
+  font-size: 26px;
+  font-family: '黑体';
+  text-align: left;
+
+  li + li {
+    margin-top: 45px;
+  }
+}
+</style>
